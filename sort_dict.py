ea7f4d79e5cd638b73a369b4bfea07b51396582c
sulagna-purkayastha <106422210+sulagna-purkayastha@users.noreply.github.com>
2022-09-01 11:03:22 +0530

    Add files via upload

---
+++ sort_dict.py
@@ -0,0 +1,12 @@
+# Q5.
+# Write a Python program to sort (ascending and descending) a dictionary by value.
+# Original dictionary : {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+# Dictionary in ascending order by value : [(0, 0), (2, 1), (1, 2), (4, 3), (3, 4)]
+# Dictionary in descending order by value : {3: 4, 4: 3, 1: 2, 2: 1, 0: 0}
+
+Original_dictionary = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+# print(sort(Original_dictionary))
+# dict1 = Original_dictionary(sorted.reverse())
+# print(dict1)
+# dict(sorted(Original_dictionary.items(), key=lambda item: item[1]))
+Dictionary in descending order by value 
